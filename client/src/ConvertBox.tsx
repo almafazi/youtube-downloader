e347d3952459240df731beb45e3309df9c78af5d
Almafazi <almafazi@icloud.com>
2024-10-10 02:45:44 +0700

    ghas

--- client/src/ConvertBox.tsx
+++ client/src/ConvertBox.tsx
@@ -12,6 +12,7 @@ import {
   MenuItem,
   MenuList,
   Stack,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { formats } from './utils/helpers';
 
@@ -21,6 +22,7 @@ interface Props {
 }
 export default function ConvertBox(props: Props) {
   const { data, chooseFormat } = props;
+
   const handleDownload = () => {
     chooseFormat(data);
   };
@@ -35,22 +37,23 @@ export default function ConvertBox(props: Props) {
         background: useColorModeValue('gray.200', 'gray.700'),
       }}
     >
-      <Box>
-        <Grid alignItems="center" gridAutoFlow="column">
-          <Image
-            borderRadius="lg"
-            src={data.thumbnail_url}
-            alt={`Thumbnail of ${data.fileMetaData?.title}`}
-          />
-          <Box p="0.5">
-            <Heading size="sm">{data.fileMetaData?.title}</Heading>
-            <Text mb="5" size="sm">{data.fileMetaData?.artist}</Text>
-            <Button leftIcon={<DownloadIcon />} onClick={handleDownload} variant='solid'>
+        <Box>
+          <Grid alignItems="center" templateColumns={{ base: '1fr', md: 'auto 1fr' }} gap={4} flexDirection={'column'}>
+            <Image
+              borderRadius="sm"
+              src={data.thumbnail_url}
+              alt={`Thumbnail of ${data.fileMetaData?.title}`}
+              width={{ base: '100%', md: 'auto' }} // Adjust width for mobile
+            />
+            <Box p="0.5">
+              <Heading size="sm">{data.fileMetaData?.title}</Heading>
+              <Text mb="5" size="sm">{data.fileMetaData?.artist}</Text>
+              <Button leftIcon={<DownloadIcon />} onClick={handleDownload} variant='solid'>
                 Download {data.format}
-            </Button>
-          </Box>
-        </Grid>
-      </Box>
+              </Button>
+            </Box>
+          </Grid>
+        </Box>
     </Box>
   );
 }
